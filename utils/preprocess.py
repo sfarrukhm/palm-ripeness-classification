1d5db7cd8b6697c3be9398246bdfd12d6ec1c6bb
Sayyed Farrukh Mehmood <153420545+sfarrukhm@users.noreply.github.com>
2025-07-24 15:08:36 +0500

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,20 +1,20 @@
-import pandas as pd
-import os
-import glob as gb
-import matplotlib.pyplot as plt
-
-def count_images(path, title):
-  df = pd.DataFrame(columns=[f"{title}_Class", "Count"])
-  for folder in os.listdir(path):
-    files = gb.glob(pathname= path + folder + "/*.jpg")
-    df.loc[len(df.index)] = [folder, len(files)]
-  return df
-
 import os
 import glob as gb
 import matplotlib.pyplot as plt
 import pandas as pd
 from tqdm import tqdm
+
+
+def count_images(path, title): 
+    df = pd.DataFrame(columns=[f"{title}_Class", "Count"])
+    
+    for folder in os.listdir(path):
+        folder_path = os.path.join(path, folder)
+        files = gb.glob(os.path.join(folder_path, "*.jpg"))
+        df.loc[len(df.index)] = [folder, len(files)]
+    
+    return df
+
 
 def get_image_size(path, is_pred=False):
     """
